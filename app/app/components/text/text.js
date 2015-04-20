dff2dbce84b6049541d87b501166f034f9e74cb6
Brandon Satrom <bsatrom@gmail.com>
2015-04-20 16:30:54 -0500

    added text details screen

--- app/app/components/text/text.js
+++ app/app/components/text/text.js
@@ -1,13 +1,32 @@
 var dialogsModule = require("ui/dialogs");
 var http = require("http");
 var spark = require("../../shared/spark-helper");
+var view = require("ui/core/view");
 
 var requestUrl = spark.baseUrl + spark.deviceId;
-var requestBody = "access_token=" + spark.accessToken + "&args={N}+GOOD!";
+var requestBody = "access_token=" + spark.accessToken + "";
 
 function pageNavigatedTo(args) {
     var page = args.object;
     page.bindingContext = page.navigationContext;
+
+    // iOS-Specific Status-Bar Work
+    if (page.ios) {
+      page.ios.title = "Display Text";
+    }
+}
+exports.pageNavigatedTo = pageNavigatedTo;
+
+function sendTapped(args) {
+  var sender = args.object;
+  var parent = sender.parent;
+
+  if (parent) {
+    var textInput = view.getViewById(parent, "displayText");
+
+    requestBody += "&args=" + textInput.text;
+
+    console.log("**** " + textInput.text);
 
     http.request({ url: requestUrl + "/setText", method: "POST",
                    headers: spark.requestHeaders,
@@ -19,5 +38,6 @@ function pageNavigatedTo(args) {
         console.log("ERROR ***** " + JSON.stringify(e));
         done(e);
     });
+  }
 }
-exports.pageNavigatedTo = pageNavigatedTo;
+exports.sendTapped = sendTapped;
--- app/app/components/weather/weather.js
+++ app/app/components/weather/weather.js
@@ -1,7 +1,7 @@
-var frames = require("ui/frame");
 var dialogsModule = require("ui/dialogs");
 var http = require("http");
 var spark = require("../../shared/spark-helper");
+var view = require("ui/core/view");
 
 var requestUrl = spark.baseUrl + spark.deviceId;
 var requestBody = "access_token=" + spark.accessToken + "";
@@ -12,27 +12,32 @@ function pageNavigatedTo(args) {
 
     // iOS-Specific Status-Bar Work
     if (page.ios) {
-      var controller = frames.topmost().ios.controller;
-      var navigationItem = controller.visibleViewController.navigationItem;
-
       page.ios.title = "Show Weather";
     }
 }
 exports.pageNavigatedTo = pageNavigatedTo;
 
 function sendTapped(args) {
-  // Get the text
-  requestBody += "&args="; //ADD CITY from args
+  var sender = args.object;
+  var parent = sender.parent;
 
-  http.request({ url: requestUrl + "/showWeather", method: "POST",
-                 headers: spark.requestHeaders,
-                 content: requestBody }).then(function (response) {
-      var statusCode = response.statusCode;
+  if (parent) {
+    var textInput = view.getViewById(parent, "cityName");
 
-      console.log("**** " + statusCode);
-  }, function (e) {
-      console.log("ERROR ***** " + JSON.stringify(e));
-      done(e);
-  });
+    requestBody += "&args=" + textInput.text;
+
+    console.log("**** " + textInput.text);
+
+    http.request({ url: requestUrl + "/showWeather", method: "POST",
+                   headers: spark.requestHeaders,
+                   content: requestBody }).then(function (response) {
+        var statusCode = response.statusCode;
+
+        console.log("**** " + statusCode);
+    }, function (e) {
+        console.log("ERROR ***** " + JSON.stringify(e));
+        done(e);
+    });
+  }
 }
 exports.sendTapped = sendTapped;
